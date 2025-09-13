3e0cb06386ebd94e2bb13e7f0a27980c8426acb9
kusum <s.kusi04@gmail.com>
2025-09-13 23:43:58 +1200

    Introduce a bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Node is fun to learn! Leaning DevOps in funway.');
+  res.send('Hello, Node is fun to learn! Leaning DevOps in funway.' + 1/0);
 });
 
 app.listen(port, () => {
